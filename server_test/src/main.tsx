f404ce6a0adab9cd372336f55fa7a4a9c7969076
Break Yang <breakds@gmail.com>
2016-08-14 10:10:15 -0400

    In preparation of the javascript test of the server. x

--- server_test/src/main.tsx
+++ server_test/src/main.tsx
@@ -1,11 +1,24 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+async function getResponse() {
+  try {
+    let response = await fetch('http://localhost:12345');
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 class TestPanel extends React.Component<{}, {}> {
   render(): JSX.Element {
     return (
-      <div>
-        <button>haha</button>
+      <div style={{margin: "50%"}}>
+        <button onClick={() => {
+            getResponse();
+          }}>
+          haha
+        </button>
       </div>
     );
   }
